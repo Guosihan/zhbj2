f9c4627af22eb3561e3c0aca392b7e911392596a
hh <hh@163.com>
2017-04-28 08:48:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gsh/zhbj2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gsh.zhbj2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gsh on 2017/4/26.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
